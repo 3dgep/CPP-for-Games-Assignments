b9be9827e08538773da464a8a0f9bff6c1f3a5a3
Jeremiah van Oosten <jeremiah@3dgep.com>
2025-03-19 10:32:36 +0100

    Add 03-Greeting.

--- 03-Greeting/main.cpp
+++ 03-Greeting/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <string>
+#include <limits>
 
 int main()
 {
+    std::string name;
+    int age;
+
+    std::cout << "What is your name?" << std::endl;
+    std::cin >> name;
+    std::cout << "What is your age?" << std::endl;
+    std::cin >> age;
+
+    while (std::cin.fail())
+    {
+        std::cout << "ERROR: I expected a number!" << std::endl;
+
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits < std::streamsize >::max(), '\n');
+
+        std::cout << "What is your age?" << std::endl;
+        std::cin >> age;
+    }
+
+    std::cout << "Hello, " << name << ". You are " << age << " years old." << std::endl;
+
     return 0;
 }
